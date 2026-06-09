dfdb3760938eda328569ff9304bb076e2d893716
Samrat-Deula7 <samratdeula461@gmail.com>
2026-06-09 13:22:08 +0545

    Learned to inject service inside controller

--- my-poadcast-api/src/episodes/episodes.controller.ts
+++ my-poadcast-api/src/episodes/episodes.controller.ts
@@ -1,30 +1,35 @@
 import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
 import { EpisodesService } from './episodes.service';
+import { type CreateEpisodeDto } from '../../entity/episode.entity';
+import { ConfigService } from 'src/config/config.service';
 
 @Controller('episodes')
 export class EpisodesController {
-  constructor(private episodesService: EpisodesService) {}
-  
+  constructor(
+    private episodesService: EpisodesService,
+    private configService: ConfigService,
+  ) {}
+
   @Get()
   findAll(@Query('sort') sort: 'asc' | 'desc' = 'desc') {
     console.log(sort);
-    return 'all episodes';
+    return this.episodesService.findAll(sort);
   }
 
   @Get('featured')
   findFeatured() {
-    return 'featured episodes';
+    return this.episodesService.findFeatured();
   }
 
   @Get(':id')
   findOne(@Param() id: string) {
     console.log(id);
-    return 'featured episodes';
+    return this.episodesService.findOne(id);
   }
 
   @Post()
-  create(@Body() input: any) {
+  create(@Body() input: CreateEpisodeDto) {
     console.log(input);
-    return 'new episode';
+    return this.episodesService.create(input);
   }
 }
