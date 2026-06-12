c627eda628d6e0095e3d7c796a95fccf732a34dd
Samrat-Deula7 <samratdeula461@gmail.com>
2026-06-12 12:09:57 +0545

    Learning about pipes in nest

--- my-poadcast-api/src/episodes/episodes.controller.ts
+++ my-poadcast-api/src/episodes/episodes.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import { Body, Controller, Get, Param, ParseIntPipe, Post, Query } from '@nestjs/common';
 import { EpisodesService } from '@episodes/episodes.service';
 import { type CreateEpisodeDto } from '../../entity/episode.entity';
 import { ConfigService } from '@config/config.service';
@@ -11,7 +11,10 @@ export class EpisodesController {
   ) {}
 
   @Get()
-  findAll(@Query('sort') sort: 'asc' | 'desc' = 'desc') {
+  findAll(
+    @Query('sort') sort: 'asc' | 'desc' = 'desc',
+    @Query('limit',ParseIntPipe) limit?: number,
+  ) {
     console.log(sort);
     return this.episodesService.findAll(sort);
   }
@@ -26,7 +29,13 @@ export class EpisodesController {
     console.log(id);
     const episode = await this.episodesService.findOne(id);
     if(!episode){
-      throw new Error("Episode not found")
+      throw new Error("Episode not found");
+
+      // Some other error
+
+      // throw new HttpException('Episode not found',HttpStatus.NOT_FOUND);
+      // throw new NotFoundException('Episode not found')
+      
     }
 
     return episode;
